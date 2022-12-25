c5719e551a257e19f923b1c742c33c211fb5c429
Denis Kozhukhar <mts701475@gmail.com>
2022-12-25 18:04:19 +0300

    Юнит тестирование

---
+++ UnitTestAworplace/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestAworplace")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestAworplace")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("27692b35-2158-4540-b1db-e93d05e88be0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestAworplace/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestAworplace
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_Method_AddReader()
+        {
+            string lastName = "Иванов";
+            string firstName = "Иван";
+            string patromymic = "Иванович";
+            DateTime dateTime = DateTime.Now;
+            int selectedItem = 1;
+            string place = "Работа";
+
+            Assert.AreEqual(lastName, firstName);
+        }
+    }
+}
